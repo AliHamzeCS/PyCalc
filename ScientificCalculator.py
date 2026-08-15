9883a63036967fe6deece00c737520177fd8dcf3
AliHamze <hamze6401@gmail.com>
2026-08-15 21:08:33 +0200

    feat: add scientific calculator v1.4.0

---
+++ ScientificCalculator.py
@@ -0,0 +1,197 @@
+import math
+import History
+
+def square_root():
+    while True:
+        try:
+            s_num = float(input('Enter a number : '))
+            
+            if s_num >= 0:   
+                result_square_root = math.sqrt(s_num)
+                print(f'Square root of {s_num} is {result_square_root}')
+                History.add_history(
+                	f'Square root of {s_num} is',
+                	result_square_root
+                )
+                break  
+                
+            else:
+                print('Enter a non-negative number')
+        
+        except ValueError:  
+            print('\nError: Please enter numbers only.')		
+
+def cube_root():
+    while True:
+        try:
+            c_num = float(input('Enter a number : '))
+            
+            result_cube_root = math.copysign(abs(c_num) ** (1 / 3), c_num)
+            print(f'Cube root of {c_num} is {result_cube_root}') 
+            History.add_history(
+            	f'Cube root of {c_num} is',
+            	result_cube_root
+            )
+            break 
+        
+        except ValueError:  
+            print('\nError: Please enter numbers only.')
+
+def sin_func(): 
+    while True: 
+        try:
+            degree_numbr = float(input('Enter angle in degrees : '))
+        
+            radian_number = math.radians(degree_numbr) 
+        
+            sin_result = math.sin(radian_number)
+        
+            print(f'sin({degree_numbr}°) = {sin_result}')
+            
+            History.add_history(
+            	f'sin({degree_numbr}°) ',
+            	sin_result
+            )
+            break
+        
+        except ValueError:
+            print('\nError: Please enter numbers only.')
+            
+def cos_func(): 
+    while True: 
+        try:
+            degree_numbr = float(input('Enter angle in degrees : '))
+        
+            radian_number = math.radians(degree_numbr) 
+        
+            cos_result = math.cos(radian_number)
+        
+            print(f'cos({degree_numbr}°) = {cos_result}')
+            History.add_history(
+            	f'cos({degree_numbr}°) ',
+            	cos_result
+            )
+            break
+        
+        except ValueError:
+            print('\nError: Please enter numbers only.')
+            
+def tan_func(): 
+    while True: 
+        try:
+            degree_numbr = float(input('Enter angle in degrees : '))
+        
+            radian_number = math.radians(degree_numbr) 
+        
+            tan_result = math.tan(radian_number)
+        
+            print(f'tan({degree_numbr}°) = {tan_result}')
+            History.add_history(
+            	f'tan({degree_numbr}°) ',
+            	tan_result
+            )
+            break
+        
+        except ValueError:
+            print('\nError: Please enter numbers only.')
+            
+def logarithm_func():
+    while True:
+        try:
+            l_num = float(input('Enter a number : '))
+            
+            if l_num > 0:   
+                result_log10= math.log10(l_num)
+                result_log= math.log(l_num)
+                
+                print(f'log10({l_num}) = {result_log10}')
+                print(f'ln({l_num}) = {result_log}')
+                History.add_history(
+                	f'log10({l_num}) ',
+                	result_log10
+                )
+                History.add_history(
+               	 f'ln({l_num}) = ',
+                	result_log
+                )
+                break  
+                
+            else:
+                print('Enter a positive number')
+        
+        except ValueError:  
+            print('\nError: Please enter numbers only.')
+            
+            
+def factorial_func():
+    while True:
+        try:
+            f_num = int(input('Enter a number : '))
+            
+            if f_num >= 0:   
+                result_factorial= math.factorial(f_num)
+                
+                
+                print(f'({f_num})! = {result_factorial}')
+                History.add_history(
+                	f'({f_num})! ',
+                	result_factorial
+                )
+                break  
+                
+            else:
+                print('Enter a positive number')
+        
+        except ValueError:  
+            print('\nError: Please enter numbers only.')
+
+def asin_func(): 
+    while True: 
+        try:
+            num = float(input('Enter value between -1 and 1 : '))
+            if -1 <= num <= 1:
+                radian_result = math.asin(num)
+                degree_result = math.degrees(radian_result)
+                print(f'arcsin({num}) = {degree_result}°')
+                History.add_history(
+                	f'arcsin({num}) ',
+                	degree_result
+                )
+                break
+            else:
+                print('Error: Value must be between -1 and 1')
+        except ValueError:
+            print('\nError: Please enter numbers only.')
+
+def acos_func(): 
+    while True: 
+        try:
+            num = float(input('Enter value between -1 and 1 : '))
+            if -1 <= num <= 1:
+                radian_result = math.acos(num)
+                degree_result = math.degrees(radian_result)
+                print(f'arccos({num}) = {degree_result}°')
+                History.add_history(
+                	f'arccos({num}) ',
+                	degree_result
+                )
+                break
+            else:
+                print('Error: Value must be between -1 and 1')
+        except ValueError:
+            print('\nError: Please enter numbers only.')
+
+def atan_func(): 
+    while True: 
+        try:
+            num = float(input('Enter a number : ')) 
+            radian_result = math.atan(num)
+            degree_result = math.degrees(radian_result)
+            print(f'arctan({num}) = {degree_result}°')
+            History.add_history(
+            	f'arctan({num}) ',
+            	degree_result
+            )
+            break
+        except ValueError:
+            print('\nError: Please enter numbers only.')
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import Calculator
 import QuadraticEquations  
 import History
 import Geometry
+import ScientificCalculator
 
 def sleep(seconds):
     time.sleep(seconds)
@@ -18,18 +19,19 @@ def clear_screen():
 PAMT1 = 'PyCalc'
 PAMT2 = 'Advanced Math Toolkit'
 
-print('=' * 40)
-print(PAMT1.center(40))
-print(PAMT2.center(40))
-print('=' * 40, end='\n\n')
 
 while True:
+    print('=' * 40)
+    print(PAMT1.center(40))
+    print(PAMT2.center(40))
+    print('=' * 40, end='\n\n')
 
     print(f"{Fore.RED}1{Style.RESET_ALL}. Calculator")
     print(f"{Fore.RED}2{Style.RESET_ALL}. Quadratic Equations")
     print(f"{Fore.RED}3{Style.RESET_ALL}. Geometry")
-    print(f"{Fore.RED}4{Style.RESET_ALL}. History")
-    print(f"{Fore.RED}5{Style.RESET_ALL}. Exit", end='\n\n')  
+    print(f"{Fore.RED}4{Style.RESET_ALL}. Scientific Calculator")
+    print(f"{Fore.RED}5{Style.RESET_ALL}. History")
+    print(f"{Fore.RED}6{Style.RESET_ALL}. Exit", end='\n\n')  
 
     try:
         choice = int(input('Choose : '))
@@ -102,21 +104,21 @@ while True:
                 clear_screen()
                 sleep(1)
                 break
-    
+
     elif choice == 2:
         while True:
             clear_screen()
             sleep(1)
-            
+
             QE = ' Quadratic Equations'
             print(QE.center(40, '=').upper(), end='\n\n')
-            
+
             print(f"{Fore.RED}1{Style.RESET_ALL}. One Quadratic Equation")
             print(f"{Fore.RED}2{Style.RESET_ALL}. System Of Two Equations")
             print(f"{Fore.RED}3{Style.RESET_ALL}. System Of Three Equations")
             print(f"{Fore.RED}4{Style.RESET_ALL}. Discriminant & Root Analysis")
             print(f"{Fore.RED}5{Style.RESET_ALL}. Back" , end='\n\n')
-            
+
             try:
                 qe_choice = int(input('Choose : '))  
 
@@ -124,35 +126,35 @@ while True:
                 print('\nError: Please enter a number.')
                 sleep(1)
                 continue
-                
+
             if qe_choice == 1:
                 clear_screen()
                 sleep(1)
                 print(f"{Fore.CYAN}-One Quadratic Equation-{Style.RESET_ALL}", end='\n\n')
                 QuadraticEquations.solve_quadratic_equation()
                 input('\nPress Enter to continue...')
-                
+
             elif qe_choice == 2:
                 clear_screen()
                 sleep(1)
                 print(f"{Fore.CYAN}-System Of Two Equations-{Style.RESET_ALL}", end='\n\n')
                 QuadraticEquations.solve_system_two_equations()
                 input('\nPress Enter to continue...')
-            
+
             elif qe_choice == 3:
                 clear_screen()
                 sleep(1)
                 print(f"{Fore.CYAN}-System Of Three Equations-{Style.RESET_ALL}", end='\n\n')
                 QuadraticEquations.solve_system_three_equations()
                 input('\nPress Enter to continue...')
-                
+
             elif qe_choice == 4:  
                 clear_screen()
                 sleep(1)
                 print(f"{Fore.CYAN}-Discriminant & Root Analysis-{Style.RESET_ALL}", end='\n\n')
                 QuadraticEquations.discriminant_analysis() 
                 input('\nPress Enter to continue...')
-            
+
             elif qe_choice == 5:
                 clear_screen()
                 sleep(1)
@@ -163,7 +165,7 @@ while True:
             sleep(1)
             geo = ' Geometry '
             print(geo.center(40, '=').upper(), end='\n\n')
-        
+
             print(f"{Fore.RED}1{Style.RESET_ALL}. Circle")
             print(f"{Fore.RED}2{Style.RESET_ALL}. Rectangle")
             print(f"{Fore.RED}3{Style.RESET_ALL}. Square")
@@ -172,96 +174,200 @@ while True:
             print(f"{Fore.RED}6{Style.RESET_ALL}. Sphere")
             print(f"{Fore.RED}7{Style.RESET_ALL}. Cylinder")
             print(f"{Fore.RED}8{Style.RESET_ALL}. Back", end='\n\n')
-            
+
             try:
                 ge_choice = int(input('Choose : '))  
-            
+
             except ValueError:
                 print('\nError: Please enter a number.')
                 sleep(1)
                 continue
-            
+
             if ge_choice == 1:
                 clear_screen()
                 sleep(1)
                 print(f"{Fore.CYAN}-Circle-{Style.RESET_ALL}", end='\n\n')
                 Geometry.circle() 
                 input('\nPress Enter to continue...')
-                
+
             elif ge_choice == 2:
                 clear_screen()
                 sleep(1)
                 print(f"{Fore.CYAN}-Rectangle-{Style.RESET_ALL}", end='\n\n')
                 Geometry.rectangle() 
                 input('\nPress Enter to continue...')
-                
+
             elif ge_choice == 3:
                 clear_screen()
                 sleep(1)
                 print(f"{Fore.CYAN}-Square-{Style.RESET_ALL}", end='\n\n')
                 Geometry.square() 
                 input('\nPress Enter to continue...')
-                
+
             elif ge_choice == 4:
                 clear_screen()
                 sleep(1)
                 print(f"{Fore.CYAN}-Triangle-{Style.RESET_ALL}", end='\n\n')
                 Geometry.triangle() 
                 input('\nPress Enter to continue...')
-                
+
             elif ge_choice == 5:
                 clear_screen()
                 sleep(1)
                 print(f"{Fore.CYAN}-Cube-{Style.RESET_ALL}", end='\n\n')
                 Geometry.cube() 
                 input('\nPress Enter to continue...')
-                
+
             elif ge_choice == 6:
                 clear_screen()
                 sleep(1)
                 print(f"{Fore.CYAN}-Sphere-{Style.RESET_ALL}", end='\n\n')
                 Geometry.sphere() 
                 input('\nPress Enter to continue...')
-                
+
             elif ge_choice == 7:
                 clear_screen()
                 sleep(1)
                 print(f"{Fore.CYAN}-Cylinder-{Style.RESET_ALL}", end='\n\n')
                 Geometry.cylinder() 
                 input('\nPress Enter to continue...')
-                
+
             elif ge_choice == 8:
                 clear_screen()
                 sleep(1)
                 break
                 
     elif choice == 4:
-        while True:
+    	while True:
             clear_screen()
             sleep(1)
+
+            scientific_calculator = ' Scientific Calculator '
+            print(scientific_calculator.center(40, '=').upper(), end='\n\n')
             
-            history = ' History '
-            print(history.center(40, '=').upper(), end='\n\n')
-            
-            print(f"{Fore.RED}1{Style.RESET_ALL}. Show History ")
-            print(f"{Fore.RED}2{Style.RESET_ALL}. Clear History")
-            print(f"{Fore.RED}3{Style.RESET_ALL}. Back", end='\n\n')
+            print(f"{Fore.RED}1{Style.RESET_ALL}. Square Root")
+            print(f"{Fore.RED}2{Style.RESET_ALL}. Cube Root")
+            print(f"{Fore.RED}3{Style.RESET_ALL}. Logarithm")
+            print(f"{Fore.RED}4{Style.RESET_ALL}. Factorial")
+            print(f"{Fore.RED}5{Style.RESET_ALL}. Sin")
+            print(f"{Fore.RED}6{Style.RESET_ALL}. Cos")
+            print(f"{Fore.RED}7{Style.RESET_ALL}. Tang")
+            print(f"{Fore.RED}8{Style.RESET_ALL}. asin")
+            print(f"{Fore.RED}9{Style.RESET_ALL}. acos")
+            print(f"{Fore.RED}10{Style.RESET_ALL}. atang")
+            print(f"{Fore.RED}11{Style.RESET_ALL}. Back", end='\n\n')
             
             try:
-                hi_choice = int(input('Choose : '))  
-            
+                Sc_choice = int(input('Choose : '))  
+
             except ValueError:
                 print('\nError: Please enter a number.')
                 sleep(1)
                 continue
-            
+
+            if Sc_choice == 1:
+                clear_screen()
+                sleep(1)
+                print(f"{Fore.CYAN}-Square Root-{Style.RESET_ALL}", end='\n\n')
+                ScientificCalculator.square_root()
+                input('\nPress Enter to continue...')
+
+            elif Sc_choice == 2:
+                clear_screen()
+                sleep(1)
+                print(f"{Fore.CYAN}-Cube Root-{Style.RESET_ALL}", end='\n\n')
+                ScientificCalculator.cube_root()
+                input('\nPress Enter to continue...')
+
+            elif Sc_choice == 3:
+                clear_screen()
+                sleep(1)
+                print(f"{Fore.CYAN}-Logarithm-{Style.RESET_ALL}", end='\n\n')
+                ScientificCalculator.logarithm_func()                
+                input('\nPress Enter to continue...')
+
+            elif Sc_choice == 4:
+                clear_screen()
+                sleep(1)
+                print(f"{Fore.CYAN}-Factorial-{Style.RESET_ALL}", end='\n\n')
+                ScientificCalculator.factorial_func()                 
+                input('\nPress Enter to continue...')
+
+            elif Sc_choice == 5:
+                clear_screen()
+                sleep(1)
+                print(f"{Fore.CYAN}-Sin-{Style.RESET_ALL}", end='\n\n')
+                ScientificCalculator.sin_func()                 
+                input('\nPress Enter to continue...')
+
+            elif Sc_choice == 6:
+                clear_screen()
+                sleep(1)
+                print(f"{Fore.CYAN}-Cos-{Style.RESET_ALL}", end='\n\n')
+                ScientificCalculator.cos_func()                
+                input('\nPress Enter to continue...')
+
+            elif Sc_choice == 7:
+                clear_screen()
+                sleep(1)
+                print(f"{Fore.CYAN}-Tang-{Style.RESET_ALL}", end='\n\n')
+                ScientificCalculator.tan_func()                 
+                input('\nPress Enter to continue...')
+                
+                
+            elif Sc_choice == 8:
+                clear_screen()
+                sleep(1)
+                print(f"{Fore.CYAN}-asin-{Style.RESET_ALL}", end='\n\n')
+                ScientificCalculator.asin_func()                 
+                input('\nPress Enter to continue...')
+
+            elif Sc_choice == 9:
+                clear_screen()
+                sleep(1)
+                print(f"{Fore.CYAN}-acos-{Style.RESET_ALL}", end='\n\n')
+                ScientificCalculator.acos_func()               
+                input('\nPress Enter to continue...')
+
+            elif Sc_choice == 10:
+                clear_screen()
+                sleep(1)
+                print(f"{Fore.CYAN}-atang-{Style.RESET_ALL}", end='\n\n')
+                ScientificCalculator.atan_func()              
+                input('\nPress Enter to continue...')
+
+            elif Sc_choice == 11:
+                clear_screen()
+                sleep(1)
+                break
+
+    elif choice == 5:
+        while True:
+            clear_screen()
+            sleep(1)
+
+            history = ' History '
+            print(history.center(40, '=').upper(), end='\n\n')
+
+            print(f"{Fore.RED}1{Style.RESET_ALL}. Show History ")
+            print(f"{Fore.RED}2{Style.RESET_ALL}. Clear History")
+            print(f"{Fore.RED}3{Style.RESET_ALL}. Back", end='\n\n')
+
+            try:
+                hi_choice = int(input('Choose : '))  
+
+            except ValueError:
+                print('\nError: Please enter a number.')
+                sleep(1)
+                continue
+
             if hi_choice == 1:
                 clear_screen()
                 sleep(1)
                 print(f"{Fore.CYAN}-Show History-{Style.RESET_ALL}", end='\n\n')
                 History.view_history()
                 input('\nPress Enter to continue...')
-                            
+
             elif hi_choice == 2:
                 clear_screen()
                 sleep(1)
@@ -273,19 +379,19 @@ while True:
                 print("History cleared successfully.")
 
                 input('\nPress Enter to continue...')
-                        
+
             elif hi_choice == 3:
                 clear_screen()
                 sleep(1)
                 break
 
-    elif choice == 5:
+    elif choice == 6:
         clear_screen()
         print('Exit ...')
         sleep(1)
         break
 
     else:
-        print('\nError: Please choose 1, 2, 3, 4, or 5.')
+        print('\nError: Please choose 1, 2, 3, 4, 5, or 6.')
         sleep(1)
         clear_screen()
